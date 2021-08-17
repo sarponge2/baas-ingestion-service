706e2c85aa6d5fbef3237a8c1b612db9b5c63ab5
sarponge2 <84182881+sarponge2@users.noreply.github.com>
2021-08-17 10:27:48 +0000

    Add files via upload

--- injest.js
+++ injest.js
@@ -1,14 +1,14 @@
 
 const {run} = require("./lib");
 
-// const users = [
-//     'DOMCY2000', 'KWEKU13', 'ABZ', 'CHEM2015', 'CYNDY77', 'ODETTEB', 'PQUANTSON10', 'GPLANGE',
-// ]
+const users = [
+    'DOMCY2000', 'KWEKU13', 'ABZ', 'CHEM2015', 'CYNDY77', 'ODETTEB', 'PQUANTSON10', 'GPLANGE',
+]
 
 // 'CAROLINE1612', 'AMIYO', 'NKA'
 
-const users = [
-    'FRANNY'
-]
+// const users = [
+//     'FRANNY'
+// ]
 
 Promise.all(users.map(run));
