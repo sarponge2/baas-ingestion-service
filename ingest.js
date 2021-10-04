7ea62fbd77c9e3092330a3fe0e4182a049d59d39
sarponge2 <84182881+sarponge2@users.noreply.github.com>
2021-10-04 17:17:13 +0000

    Add files via upload

---
+++ ingest.js
@@ -0,0 +1,39 @@
+const commandLineArgs = require('command-line-args')
+const commandLineUsage = require('command-line-usage')
+const {admin, run} = require("./lib");
+const {env} = require("fetch/.eslintrc");
+
+const optionList = [
+    {name: 'employee', alias: 'e', description: 'Whether the user to ingest is an employee', type: Boolean},
+    {
+        name: 'cluster',
+        alias: 'c',
+        defaultOption: 'dev',
+        typeLabel: 'one of ({underline dev}, {underline stg}, {underline prd})',
+        description: 'the cluster to ingest user into'
+    },
+    {name: 'user', alias: 'u', description: 'Username of user to ingest', defaultOption: true},
+    {name: 'help', alias: 'h', description: 'Show this help', type: Boolean},
+]
+
+const options = commandLineUsage([
+    {
+        header: 'Backbase OCH User Ingestion',
+        content: 'Backbase OCH User Ingestion utility script'
+    },
+    {
+        header: 'Options',
+        optionList
+    }
+])
+
+args = commandLineArgs(optionList)
+
+args.env = args.env || 'dev'
+
+if (!Object.keys(args).length || args.help || !args.user) console.log(options)
+
+if (args.employee) admin(args.user, args.env)
+else run(args.user, args.env)
+
+console.log(args)
